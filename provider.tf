0c15765718c6f19ea8d02348db1c0e6d651b34ab
Admir Trakic <xomodo@gmail.com>
2019-01-10 15:12:44 +0100

    Add provider

---
+++ provider.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 0.11.10"
+}
+
+provider "external" {
+  version = "1.0.0"
+}
+
+provider "local" {
+  version = "1.0.0"
+}
+
+provider "null" {
+  version = "1.0.0"
+}
+
+provider "random" {
+  version = "1.0.0"
+}
+
+provider "template" {
+  version = "1.0.0"
+}
+
+#provider "tls" {
+#  version = "1.0.0"
+#}
